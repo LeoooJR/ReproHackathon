35c1748a852d0c499275421e9b3c2ccd32e37e5a
Leo <leo.jourdain@universite-paris-saclay.fr>
2024-11-05 20:14:30 +0100

    [UPDATE] relative path to config file

--- workflows/workflow.sh
+++ workflows/workflow.sh
@@ -2,7 +2,7 @@
 
 # Variables
 PROFILE='standard,container'
-CONFIG=/ifb/data/mydatalocal/Next/ReproHackathon/workflows/config/nextflow.config
+CONFIG=./config/nextflow.config
 DOWNLOAD=true
 REF="https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=CP000253.1&rettype=fasta"
 GFF=""
