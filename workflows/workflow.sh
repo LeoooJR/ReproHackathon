e75e7636953d9ac7870689e145967febf84a4fe5
Leo <leo.jourdain@universite-paris-saclay.fr>
2024-11-07 23:49:36 +0100

    [UPDATE] make modifs to work with new DESeq2 image

--- workflows/workflow.sh
+++ workflows/workflow.sh
@@ -6,11 +6,11 @@ CONFIG=./config/nextflow.config
 DOWNLOAD=true
 REF="https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=CP000253.1&rettype=fasta"
 GFF=""
-IMAGES_NAMES=('ubuntu.sif' 'sratoolkit.sif' 'fastqc.sif' 'trimgalore.sif' 'bowtie.sif' 'samtools.sif' 'featurecounts.sif' 'deseq2.sif')
+IMAGES_NAMES=('ubuntu.sif' 'sratoolkit.sif' 'fastqc.sif' 'trimgalore.sif' 'bowtie.sif' 'samtools.sif' 'featurecounts.sif' 'DESeq2.sif')
 IMAGES_PATH=("https://storage.googleapis.com/rrnaseq/ubuntu.sif" "https://storage.googleapis.com/rrnaseq/sratoolkit.sif" \
             "https://storage.googleapis.com/rrnaseq/fastqc.sif" "https://storage.googleapis.com/rrnaseq/trimgalore.sif" \
             "https://storage.googleapis.com/rrnaseq/bowtie.sif" "https://storage.googleapis.com/rrnaseq/samtools.sif" \
-            "https://storage.googleapis.com/rrnaseq/featurecounts.sif" "https://storage.googleapis.com/rrnaseq/deseq2.sif")
+            "https://storage.googleapis.com/rrnaseq/featurecounts.sif" https://storage.googleapis.com/rrnaseq/DESeq2.sif)
 BUCKET="https://storage.googleapis.com/rrnaseq/"
 MD5SUM_FILE=".MD5SUM"
 PROCESS_FILES=("main.nf" "tools.nf" "downloadfastq.nf" "fastqc.nf" "trimgalore.nf" \
